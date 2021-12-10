27a2e9e0d3bed9e838c7fdadbda81659f753d2e4
Qian Gao <68983944+gaoqian310@users.noreply.github.com>
2021-12-09 22:56:11 -0700

    Add files via upload

---
+++ Churn_Prediction.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## REQUIRED LIBRARIES
+# For data wrangling 
+import numpy as np
+import pandas as pd
+
+# For visualization
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import seaborn as sns
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
+
+
+# In[2]:
+
+
+# Read the data frame
+df = pd.read_csv('Churn_Modelling.csv', delimiter=',')
+df.shape
+
+
+# In[3]:
+
+
+# Check columns list and missing values
+df.isnull().sum()
+
+
+# In[4]:
+
+
+# Get unique count for each variable
+df.nunique()
+
+
+# In[ ]:
+
+
+
+
